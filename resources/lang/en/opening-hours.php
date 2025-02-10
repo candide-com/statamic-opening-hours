09551ff9fa1c042d16cb70954520a5d33d0c1b74
l_kiernan <lee.kiernan@candide.com>
2025-02-10 16:32:46 +0000

    Tidy lang

--- resources/lang/en/opening-hours.php
+++ resources/lang/en/opening-hours.php
@@ -7,24 +7,6 @@ return [
     | Opening hours
     |--------------------------------------------------------------------------
     */
-
-    'opening-hours' => 'Opening hours',
-    'default-hours' => [
-        'display' => 'Default hours',
-        'instructions' => 'Set the opening hours per weekday. Multiple hour ranges are possible.<br>A weekday without opening hours is considered as closing day.'
-    ],
-    'weekdays' => [
-        'monday' => 'Monday',
-        'tuesday' => 'Tuesday',
-        'wednesday' => 'Wednesday',
-        'thursday' => 'Thursday',
-        'friday' => 'Friday',
-        'saturday' => 'Saturday',
-        'sunday' => 'Sunday',
-    ],
-    'from' => 'From',
-    'to' => 'To',
-    'date' => 'Date',
     'exceptions' => [
         'display' => 'Exceptions',
         'instructions' => 'Set exceptional closing days.<br>It is possible to set optional exceptional opening hours per closing day.',
